3c17cd7edb3ccd0c255f04f04bb6aece62ecc4b3
carsoncoleman-51 <carsoncoleman51@gmail.com>
2026-02-08 09:42:10 -0700

    added NavLink and Route

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
 
 export default function App() {
   return (
-    <div>
-        <header> 
-            <h1 id="game-title">THE <span class="red-word">RED</span> BUTTON GAME</h1>
-            <nav>
-                <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="play.html">Play</a></li>
-                <li><a href="scores.html">Scores</a></li>
-                </ul>
-            </nav>
-        </header>
+    <BrowserRouter>
+        <div>
+            <header> 
+                <h1 id="game-title">THE <span class="red-word">RED</span> BUTTON GAME</h1>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/">Login</NavLink></li>
+                        <li><NavLink to="/play">Play</NavLink></li>
+                        <li><NavLink to="/scores">Scores</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
 
-        <main> 
-        
-        </main>
+            <main> 
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/play" element={<Play />} />
+                    <Route path="/scores" element={<Scores />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
 
-        <footer> 
-            <span> Carson Coleman </span>
-            <a href="https://github.com/carsoncoleman-51/CS260-project">GitHub</a>
-        </footer>
-    </div>);
+            <footer> 
+                <span> Carson Coleman </span>
+                <a href="https://github.com/carsoncoleman-51/CS260-project">GitHub</a>
+            </footer>
+        </div>
+    </BrowserRouter>
+    );
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
