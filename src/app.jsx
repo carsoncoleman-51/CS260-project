ec74762cdfcb76117f89806a3fc9cd46fb50f956
carsoncoleman-51 <carsoncoleman51@gmail.com>
2026-02-08 09:35:46 -0700

    now have app.css and app.jsx and index.html and index.jsx

---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return (
+    <div>
+        <header> 
+            <h1 id="game-title">THE <span class="red-word">RED</span> BUTTON GAME</h1>
+            <nav>
+                <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="play.html">Play</a></li>
+                <li><a href="scores.html">Scores</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main> 
+        
+        </main>
+
+        <footer> 
+            <span> Carson Coleman </span>
+            <a href="https://github.com/carsoncoleman-51/CS260-project">GitHub</a>
+        </footer>
+    </div>);
+}
