b6085981405b3b5b3928b446e907b37d4493427c
carsoncoleman-51 <carsoncoleman51@gmail.com>
2026-03-14 19:43:39 -0600

    new deploy script and used. works

---
+++ deployService.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+while getopts k:h:s: flag
+do
+    case "${flag}" in
+        k) key=${OPTARG};;
+        h) hostname=${OPTARG};;
+        s) service=${OPTARG};;
+    esac
+done
+
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
+    printf "\nMissing required parameter.\n"
+    printf "  syntax: deployService.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    exit 1
+fi
+
+printf "\n----> Deploying service bundle $service to $hostname with $key\n"
+
+# Step 1
+printf "\n----> Build the distribution package\n"
+rm -rf build
+mkdir build
+npm install
+npm run build
+cp -rf dist build/public
+cp service/*.js build
+cp service/*.json build
+
+# Step 2
+printf "\n----> Clearing out previous distribution on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+rm -rf services/${service}
+mkdir -p services/${service}
+ENDSSH
+
+# Step 3
+printf "\n----> Copy the distribution package to the target\n"
+scp -r -i "$key" build/* ubuntu@$hostname:services/$service
+
+# Step 4
+printf "\n----> Deploy the service on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+bash -i
+cd services/${service}
+npm install
+pm2 restart ${service}
+ENDSSH
+
+# Step 5
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf build
+rm -rf dist
