c8093c0fbc405f2bdee36c9c2649fbfa37c5cb9c
carsoncoleman-51 <carsoncoleman51@gmail.com>
2026-03-26 10:25:55 -0600

    revert: remove deploy script changes

--- deployStartup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-key="${DEPLOY_KEY:-$HOME/.ssh/carson.aws.pem}"
-hostname="${DEPLOY_HOST:-theredbuttongame.click}"
-service="${DEPLOY_SERVICE:-startup}"
-
-printf "\nUsing startup deploy defaults (override with DEPLOY_KEY, DEPLOY_HOST, DEPLOY_SERVICE).\n"
-bash ./deployService.sh -k "$key" -h "$hostname" -s "$service"
